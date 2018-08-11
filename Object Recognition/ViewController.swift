73b2b4d92511d43cf18f837e2255c87e3da31c24
shahparshva72 <shahparshva72@gmail.com>
2018-08-11 13:16:50 +0530

    AVFoundation Capture Input and Output

--- Object Recognition/ViewController.swift
+++ Object Recognition/ViewController.swift
@@ -2,17 +2,39 @@
 //  ViewController.swift
 //  Object Recognition
 //
-//  Created by CSPIT-IT on 11/08/18.
+//  Created by Parshva Shah on 11/08/18.
 //  Copyright © 2018 Parshva. All rights reserved.
 //
 
 import UIKit
+import AVKit
+import Vision
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        guard let input  = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        view.layer.addSublayer(previewLayer)
+        captureSession.startRunning()
+        
+        previewLayer.frame = view.frame
+        let dataOutput = AVCaptureVideoDataOutput()
+        captureSession.addOutput(dataOutput)
+        
+        //let request = VNCoreMLRequest(model: <#T##VNCoreMLModel#>, completionHandler: <#T##VNRequestCompletionHandler?##VNRequestCompletionHandler?##(VNRequest, Error?) -> Void#>)
+        //VNImageRequestHandler(cgImage: CGImage,options:[:].perform[requests:VNRequest[])
     }
 
     override func didReceiveMemoryWarning() {
